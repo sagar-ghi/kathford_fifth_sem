568a7ccea51d4e1dca49291f847ddb5b27435781
Sagar <sagar.ghimire@kotuko.it>
2021-06-08 12:26:10 +0545

    🔥🔥 day 6 array and function

---
+++ function/arrow.js
@@ -0,0 +1,18 @@
+const add = (num1, num2) => {
+    const result = num1 + num2
+    return result
+}
+
+const output = add(3, 4)
+console.log(output)
+
+// [] bracket
+// {} braces
+// () parenthesis
+
+// if one line statement
+//     then no need of braces and return 
+const add2 = (num1, num2) => num1 + num2
+
+const output2 = add2(5, 5)
+console.log(output2)
---
+++ function/basic.js
@@ -0,0 +1,25 @@
+//function declaration
+
+function add(num1, num2) {
+    //operations"
+    const result = num1 + num2
+    return result
+    // return
+}
+const output = add(1, 2)
+// console.log(output);
+
+
+const subtract = function () {
+    console.log("I'm subtract function")
+}
+
+const name = function mult() {
+    console.log('This is mult way')
+}
+
+// subtract()
+console.log(name())
+
+// const divide = subtract
+// divide()
